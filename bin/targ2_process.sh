051b6f0b59aaf6698c75105355829d47a840be76
Frits Sweijen <tikk3r@users.noreply.github.com>
2019-10-28 17:45:36 +0100

    Bind singularity paths

--- bin/targ2_process.sh
+++ bin/targ2_process.sh
@@ -5,7 +5,8 @@ function runtaql(){
     echo "running taql on "$( ls -d ${RUNDIR}/Input/*${OBSID}*SB*  )"/SPECTRAL_WINDOW"
     if  [ ! -z ${SIMG+x} ]; then
         MS=$(ls -d ${RUNDIR}/Input/*${OBSID}*SB*)
-        FREQ=$(singularity exec $SIMG taql select distinct REF_FREQUENCY from $MS::SPECTRAL_WINDOW | tail -n 1 | head -n 1)
+        singularity exec -B /scratch,$PWD $SIMG taql select distinct REF_FREQUENCY from $MS::SPECTRAL_WINDOW
+        FREQ=$(singularity exec -B /scratch,$PWD $SIMG taql select distinct REF_FREQUENCY from $MS::SPECTRAL_WINDOW | tail -n 1 | head -n 1)
     else
         FREQ=$( echo "select distinct REF_FREQUENCY from $( ls -d ${RUNDIR}/Input/*${OBSID}*SB*  )/SPECTRAL_WINDOW"| taql | tail -1 | head -1)
     fi
